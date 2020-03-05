a59ab63660f275b903aa0c61be608f2d8ddb78d2
Jean Nunes <jeannobreganunes@hotmail.com>
2020-03-04 22:15:49 -0300

    Zumbi atacando

---
+++ ApocalypseZumbiUnity/Assets/Scripts/Bala.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward * velocidade * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter(Collider objetoColidido)
+    {
+        if (objetoColidido.gameObject.tag == "Inimigo")
+        {
+            Destroy(objetoColidido.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ ApocalypseZumbiUnity/Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour
+{
+    public GameObject jogador;
+    public float velocidade = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+        Vector3 direcao = jogador.transform.position - transform.position;
+
+        if (distancia > 2.5)
+        {
+            GetComponent<Animator>().SetBool("Atacando", false);
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * velocidade * Time.deltaTime);
+
+            Quaternion novaRotacao;
+            novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Atacando", true);
+        }
+    }
+}
